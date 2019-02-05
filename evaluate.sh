fb23e8e9dfb623287e4b3fbe3dc3f13951acd89e
Manuel Eusebio de Paz Carmona <Pelirrojo@users.noreply.github.com>
2019-02-05 23:10:41 +0100

    Add some scripts to quick test locally and clean previous models by promt.

--- evaluate.sh
+++ evaluate.sh
@@ -3,8 +3,8 @@
 python --version >/dev/null 2>&1 || { echo >&2 "I require Python but it's not installed. Aborting."; exit 1; }
 pip --version >/dev/null 2>&1 || { echo >&2 "I require PyPI but it's not installed. Aborting."; exit 1; }
 
-read -p "Please type model file (placed at ./models/model_epX), followed by [ENTER]:" MODEL_FILENAME
-read -p "Please type filename.csv (without .csv, placed at ./data/*.csv), followed by [ENTER]:" DATA_FILENAME
+read -p "Please type model file (placed at ./models/model_epX), followed by [ENTER]: " MODEL_FILENAME
+read -p "Please type filename.csv (without .csv, placed at ./data/*.csv), followed by [ENTER]: " DATA_FILENAME
 
 clear
 
