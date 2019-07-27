0424b025e278700d73c49b846964403e48325a3b
Garfield <chimera62@msn.com>
2019-07-27 09:55:52 +0800

    modify for python 3.6

--- evaluate.py
+++ evaluate.py
@@ -6,7 +6,7 @@ from functions import *
 import sys
 
 if len(sys.argv) != 3:
-	print "Usage: python evaluate.py [stock] [model]"
+	print("Usage: python evaluate.py [stock] [model]")
 	exit()
 
 stock_name, model_name = sys.argv[1], sys.argv[2]
@@ -22,7 +22,7 @@ state = getState(data, 0, window_size + 1)
 total_profit = 0
 agent.inventory = []
 
-for t in xrange(l):
+for t in range(l):
 	action = agent.act(state)
 
 	# sit
@@ -31,19 +31,19 @@ for t in xrange(l):
 
 	if action == 1: # buy
 		agent.inventory.append(data[t])
-		print "Buy: " + formatPrice(data[t])
+		print("Buy: " + formatPrice(data[t]))
 
 	elif action == 2 and len(agent.inventory) > 0: # sell
 		bought_price = agent.inventory.pop(0)
 		reward = max(data[t] - bought_price, 0)
 		total_profit += data[t] - bought_price
-		print "Sell: " + formatPrice(data[t]) + " | Profit: " + formatPrice(data[t] - bought_price)
+		print("Sell: " + formatPrice(data[t]) + " | Profit: " + formatPrice(data[t] - bought_price))
 
 	done = True if t == l - 1 else False
 	agent.memory.append((state, action, reward, next_state, done))
 	state = next_state
 
 	if done:
-		print "--------------------------------"
-		print stock_name + " Total Profit: " + formatPrice(total_profit)
-		print "--------------------------------"
+		print("--------------------------------")
+		print(stock_name + " Total Profit: " + formatPrice(total_profit))
+		print("--------------------------------")
--- functions.py
+++ functions.py
@@ -24,7 +24,7 @@ def getState(data, t, n):
 	d = t - n + 1
 	block = data[d:t + 1] if d >= 0 else -d * [data[0]] + data[0:t + 1] # pad with t0
 	res = []
-	for i in xrange(n - 1):
+	for i in range(n - 1):
 		res.append(sigmoid(block[i + 1] - block[i]))
 
 	return np.array([res])
