aacddefee33c524bad1eab2d8c6b040e9f614bb9
minzzii-kim <minzzii.kim89@gmail.com>
2023-10-02 12:00:34 +0900

    bug fixed

--- agent/agent.py
+++ agent/agent.py
@@ -23,7 +23,7 @@ class Agent:
 		self.epsilon_min = 0.01
 		self.epsilon_decay = 0.995
 
-		self.model=load_model("models/"+model_name) if not is_eval else self._model()
+		self.model=load_model("models/"+model_name) if is_eval else self._model()
 		# if not is_eval and path.exists("models/model_ep40"):
 		# 	self.model=load_model("models/model_ep40")
 		# elif is_eval:
